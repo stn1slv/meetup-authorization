51739db1fe09ce885e222f228eb4ae4b8573014e
Stanislav Deviatov <devyatov@gmail.com>
2021-07-15 15:45:20 +0300

    Create scope added

--- service-a/src/main/java/com/example/demo/DemoApplication.java
+++ service-a/src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class DemoApplication {
