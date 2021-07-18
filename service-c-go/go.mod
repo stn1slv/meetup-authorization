2302eae13b4b974a91d2dfb1ee08dd88b31fdb27
Stanislav Deviatov <devyatov@gmail.com>
2021-07-18 17:00:42 +0300

    Service-C updated

--- service-c-go/go.mod
+++ service-c-go/go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/Shopify/sarama v1.29.1
 	github.com/damiannolan/sasl v1.0.0
+	github.com/go-chi/chi/v5 v5.0.3
 )
--- service-c-go/main.go
+++ service-c-go/main.go
@@ -1,18 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"strconv"
 	"strings"
 
-	"github.com/Shopify/sarama"
+	"math/rand"
+	"net/http"
+
+	sarama "github.com/Shopify/sarama"
 	oauth "github.com/damiannolan/sasl/oauthbearer"
+	chi "github.com/go-chi/chi/v5"
 )
 
 func main() {
+	r := chi.NewRouter()
+	// r.Use(middleware.Logger)
+	r.Get("/send2http", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("sent to HTTP"))
+	})
+	r.Get("/send2kafka", func(w http.ResponseWriter, r *http.Request) {
+		requestId := rand.Intn(9000) + 999
+		msg := "test message with id=" + strconv.Itoa(requestId) + " from service-c"
+
+		_, _, err := send2kafka(msg)
+		w.Header().Add("Content-Type", "text/plain")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("ERROR: " + err.Error()))
+		} else {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("SENT: " + msg))
+		}
+	})
+	log.Fatal(http.ListenAndServe(":8383", r))
+}
+
+func send2kafka(msg string) (int32, int64, error) {
 	clientID := "service-c"
 	clientSecret := "service-c-secret"
 	tokenURL := "http://keycloak:8080/auth/realms/meetup/protocol/openid-connect/token"
 	splitBrokers := strings.Split("localhost:9092", ",")
+	topic := "a_messages"
 
 	config := sarama.NewConfig()
 
@@ -27,15 +56,18 @@ func main() {
 
 	syncProducer, err := sarama.NewSyncProducer(splitBrokers, config)
 	if err != nil {
-		fmt.Println("failed to create producer: ", err)
+		log.Println("failed to create producer: ", err)
+		return 0, 0, err
 	}
+	defer syncProducer.Close()
 	partition, offset, err := syncProducer.SendMessage(&sarama.ProducerMessage{
-		Topic: "a_messages",
-		Value: sarama.StringEncoder("test_message"),
+		Topic: topic,
+		Value: sarama.StringEncoder(msg),
 	})
 	if err != nil {
-		fmt.Printf("failed to send message to a_messages: %e", err)
+		log.Printf("failed to send message to %s: %e\n", topic, err)
+		return 0, 0, err
 	}
-	fmt.Printf("wrote message at partition: %d, offset: %d", partition, offset)
-	_ = syncProducer.Close()
+	log.Printf("wrote message at partition: %d, offset: %d\n", partition, offset)
+	return partition, offset, nil
 }
